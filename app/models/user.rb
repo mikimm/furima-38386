e3a8856065b1f43bb989756e763d8bb888ddb2b1
mikihiro makino <16id014mmiki@gmail.com>
2022-09-21 12:32:35 +0900

    ビューファイル修正・トップページ機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-
+         validates:nickname, presence: true
+         validates:lastname, presence: true
+         validates:firstname, presence: true        
+         validates:lastname_kana,presence: true  
+         validates:firstname_kana,presence: true 
+         validates:date_of_birth,presence: true       
+         has_many :items
+         has_many :orders
 end
