42dd4413f20ba46ba0e7eb491209600b0a9830c1
mikihiro makino <16id014mmiki@gmail.com>
2022-09-27 15:50:34 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user != @item.user
+      redirect_to action: :index
+    end
   end
 
   def update
