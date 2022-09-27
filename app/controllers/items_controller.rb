fc5dd50a2426c517a6156dfb2c0fae2bef11aca8
mikihiro makino <16id014mmiki@gmail.com>
2022-09-27 14:34:11 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,9 +28,9 @@ class ItemsController < ApplicationController
   def update
     @item=Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to items_path
+      redirect_to item_path
     else
-      render action: :edit
+      render action: :new
     end
   end
   private
