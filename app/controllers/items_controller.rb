8b2f77b12b9ccedac779f5388d9956490b09aae0
mikihiro makino <16id014mmiki@gmail.com>
2022-09-27 16:15:11 +0900

    リファタリング修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
+  before_action :set_item, only: [:show,:edit,:update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -18,18 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user != @item.user
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,5 +40,8 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :price, :condition_id, :burden_id, :region_id,
                                  :shippment_days_id).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
