ef30768e137538ec356abb43df59a9ab99d2b54a
mikihiro makino <16id014mmiki@gmail.com>
2022-09-27 20:36:11 +0900

    編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update
+    if @item.update(item_params)
       redirect_to item_path
     else
       render action: :edit
