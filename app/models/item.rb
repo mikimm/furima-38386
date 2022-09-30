dc1c0d3fa010b6d09745c8392a83edc661d8a8d5
mikihiro makino <111714413+mikimm@users.noreply.github.com>
2022-09-30 22:19:16 +0900

    商品購入保存機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,7 +29,7 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
-
+  has_one :order
   private
 
   def item_params
---
+++ app/models/order_address.rb
@@ -0,0 +1,18 @@
+class OrderAddress
+    include ActiveModel::Model
+    attr_accessor :post_number,:region_id,:municipalities,:address,:building,:phone_number,:user_id,:item_id,:order_id
+
+    with_options presence: true do
+        validates :post_number,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+        validates :region_id
+        validates :municipalities
+        validates :address
+        validates :phone_number,format: {with: /\A\d{10,11}\z/, message: "is invalid."}
+        validates :user_id
+        validates :item_id
+    end
+    def save
+        order = Order.create(user_id: user_id, item_id: item_id)
+        Address.create(post_number:post_number,region_id:region_id,municipalities:municipalities,address:address,building:building,phone_number:phone_number,order_id:order.id)
+    end
+end
