ebbd3c772bdefa5b2c46c8b9a367ce65c0d9a845
mikihiro makino <111714413+mikimm@users.noreply.github.com>
2022-10-03 17:01:43 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,16 +3,18 @@ before_action :authenticate_user!, except: :index
     def index
       @item = Item.find(params[:item_id])
       @order_address = OrderAddress.new
+      if @item.user_id == current_user.id&&@item.id=@order_address
+        redirect_to root_path
+      end
+      
     end
 
     def new
-      @order_address = OrderAddress.new
     end
 
     def create
       @item = Item.find(params[:item_id])
       @order_address = OrderAddress.new(order_address_params)
-
       if @order_address.valid?
         pay_item
         @order_address.save
