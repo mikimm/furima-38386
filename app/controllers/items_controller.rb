48f93ca611bc2d43da4b5ae7360e160f4641e474
mikihiro makino <111714413+mikimm@users.noreply.github.com>
2022-10-04 18:28:58 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    redirect_to root_path if @item.user != current_user
   end
 
   def update
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
   def index
     @order_address = OrderAddress.new
@@ -8,6 +8,9 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_address_params)
+    if @item.id == @order.item_id
+      redirect_to root_path
+    end
     if @order_address.valid?
       pay_item
       @order_address.save
